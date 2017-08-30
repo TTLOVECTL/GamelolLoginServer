a4e715dff176959bcb1cc8ea82f73e1886dfb916
taoting <578467957@qq.com>
2017-08-30 17:26:02 +0800

    Signed-off-by: taoting <578467957@qq.com>

---
+++ GamelolLoginServer/DataMessage/PlayerInscriptionMessage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamelolLoginServer.DataMessage
+{
+    /// <summary>
+    /// 玩家拥有的符文信息
+    /// </summary>
+    public class PlayerInscriptionMessage
+    {
+        private int _playerId;
+
+        private int _inscriptionId;
+
+        private int _inscriptionNumber;
+
+        private int _inscriptionUserNumber;
+
+        /// <summary>
+        /// 玩家Id
+        /// </summary>
+        public int PlayerId {
+            get => _playerId;
+            set => _playerId = value;
+        }
+
+        /// <summary>
+        /// 符文ID
+        /// </summary>
+        public int InscriptionId {
+            get => _inscriptionId;
+            set => _inscriptionId = value;
+        }
+
+        /// <summary>
+        /// 符文数量
+        /// </summary>
+        public int InscriptionNumber {
+            get => _inscriptionNumber;
+            set => _inscriptionNumber = value;
+        }
+
+        /// <summary>
+        /// 当前符文的使用数量
+        /// </summary>
+        public int InscriptionUserNumber {
+            get => _inscriptionUserNumber;
+            set => _inscriptionUserNumber = value;
+        }
+    }
+}
